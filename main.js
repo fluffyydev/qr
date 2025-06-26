4b246d20063030acc60ea61e564c5a7465c05aac
fluffy <121191983+fluffyydev@users.noreply.github.com>
2025-06-26 11:21:29 -0400

    Convert to Binary function Finished
    The inputted string will now be converted to the binary number of the ASCII character.

--- main.js
+++ main.js
@@ -4,14 +4,28 @@ var generateButton = document.getElementById("generateButton");
 
 
 generateButton.onclick = function() {
-  var link = urlInput.value;
-  
-  let encoder = new TextEncoder();
-  link = encoder.encode(link);
-  res.textContent = newLink;
-  
+  var binaryString = convertToBinary(urlInput.value);
+  res.textContent = binaryString;
 }
-
-function convertToBytes() {
   
+
+function convertToBinary(stringToConvert) {
+  let encoder = new TextEncoder();
+  var newLink = encoder.encode(stringToConvert);
+  
+  var resultString = "";
+  
+  for (let x = newLink.length; x >= 0; x--) {
+    var currentVal = newLink[x];
+    var binaryValue = "";
+    
+    // Section gotten from geeksforgeeks.org
+    for (let j = 7; j >= 0; j--) {
+      binaryValue += (currentVal >> j) & 1;
+    }
+    
+    resultString += binaryValue + " ";
+    // End of section 
+  }
+  return resultString;
 }
