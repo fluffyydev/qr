a91de3b5c1d2c71b0bd7e10adaebb6d4132a1ab1
fluffy <121191983+fluffyydev@users.noreply.github.com>
2025-06-26 16:15:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var res = document.getElementById("res");
 var urlInput = document.getElementById("linkInput");
 var generateButton = document.getElementById("generateButton");
 
+var quietZoneColor = "rgb(158, 158, 158)";
+var quietZoneSize = 10; // How many pixels the border goes out from QR code
 
 generateButton.onclick = function() {
   var binaryString = convertToBinary(urlInput.value);
@@ -11,12 +13,38 @@ generateButton.onclick = function() {
   const ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   
+  
   // Way to change fill color --  ctx.fillStyle = rbgColor;
   
-  ctx.fillStyle = "rgb(158, 158, 158)";
-  ctx.fillRect(0, 0, 500, 500);
+  ctx.fillStyle = quietZoneColor;
+  ctx.fillRect(0, 0, 210 + quietZoneSize * 2, 210 + quietZoneSize * 2);
+  
+  ctx.fillStyle = "rgb(255, 255, 255)";
+  ctx.fillRect(quietZoneSize, quietZoneSize, 210, 210);
+  
+  ctx.fillStyle = "rgb(0, 0, 0)";
+  
+  var qrArray = [];
+  
+  var emptyRow = [];
+    
+  for (let y = 20, y >= 0, y--) {
+    emptyRow.push(0);
+  }
+  
+  for (let b = 20, b >= 0, b--) {
+    qrArray.push(emptyRow);
+  }
+  
+  res.textContent = binaryString;
+  
+  // Positioning Squares
+  
+  
+  
 }
   
+function fillSquare(x, y) {}
 
 function convertToBinary(stringToConvert) {
   let encoder = new TextEncoder();
@@ -24,7 +52,7 @@ function convertToBinary(stringToConvert) {
   
   var resultString = "";
   
-  for (let x = newLink.length; x >= 0; x--) {
+  for (let x = newLink.length - 1; x >= 0; x--) {
     var currentVal = newLink[x];
     var binaryValue = "";
     
