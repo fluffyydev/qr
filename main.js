24943107b307da168f603bd6acdc3bd7df140543
fluffy <121191983+fluffyydev@users.noreply.github.com>
2025-06-27 01:51:12 -0400

    THE TILES WORK RAHHHH

--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 var res = document.getElementById("res");
 var urlInput = document.getElementById("linkInput");
 var generateButton = document.getElementById("generateButton");
+var canvas = document.getElementById('theCanvas');
+var ctx = canvas.getContext("2d");
 
 var quietZoneColor = "rgb(158, 158, 158)";
 var quietZoneSize = 10; // How many pixels the border goes out from QR code
 
 generateButton.onclick = function() {
   var binaryString = convertToBinary(urlInput.value);
-  res.textContent = binaryString;
-  
-  const canvas = document.getElementById('theCanvas');
-  const ctx = canvas.getContext("2d");
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   
   
@@ -26,25 +25,69 @@ generateButton.onclick = function() {
   
   var qrArray = [];
   
-  var emptyRow = [];
-    
-  for (let y = 20, y >= 0, y--) {
-    emptyRow.push(0);
+  for (let b = 20; b >= 0; b--) {
+    qrArray.push([]);
+    for (let y = 20; y >= 0; y--) {
+      qrArray[20- b].push(0);
+    }
   }
-  
-  for (let b = 20, b >= 0, b--) {
-    qrArray.push(emptyRow);
-  }
-  
-  res.textContent = binaryString;
   
   // Positioning Squares
   
+  qrArray[0][0] = 1;
+  qrArray[1][0] = 1;
+  qrArray[2][0] = 1;
+  qrArray[3][0] = 1;
+  qrArray[4][0] = 1;
+  qrArray[5][0] = 1;
+  qrArray[6][0] = 1;
+  qrArray[0][1] = 1;
+  qrArray[6][1] = 1;
+  qrArray[0][2] = 1;
+  qrArray[2][2] = 1;
+  qrArray[3][2] = 1;
+  qrArray[4][2] = 1;
+  qrArray[6][2] = 1;
+  qrArray[0][3] = 1;
+  qrArray[2][3] = 1;
+  qrArray[3][3] = 1;
+  qrArray[4][3] = 1;
+  qrArray[6][3] = 1;
+  qrArray[0][4] = 1;
+  qrArray[2][4] = 1;
+  qrArray[3][4] = 1;
+  qrArray[4][4] = 1;
+  qrArray[6][4] = 1;
+  qrArray[0][5] = 1;
+  qrArray[6][5] = 1;
+  qrArray[0][6] = 1;
+  qrArray[1][6] = 1;
+  qrArray[2][6] = 1;
+  qrArray[3][6] = 1;
+  qrArray[4][6] = 1;
+  qrArray[5][6] = 1;
+  qrArray[6][6] = 1;
+
+  makeQR(qrArray);
+}
+
+function makeQR(qrList) {
+  var size = qrList[0].length;
   
+  for (let t = 0; t <= size - 1; t++) {
+    for (let v = 0; v <= size - 1; v++) {
+      if (qrList[t][v] == 1) {
+        fillSquare(t, v);
+      }
+    }
+  }
   
 }
   
-function fillSquare(x, y) {}
+function fillSquare(x, y) {
+  ctx.fillStyle = "rgb(0, 0, 0)";
+  ctx.fillRect(quietZoneSize + 10 * x, quietZoneSize + 10 * y, 10, 10);
+}
 
 function convertToBinary(stringToConvert) {
   let encoder = new TextEncoder();
